ed6ac98b46c85d3043c7f29526c605762380d2a6
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-07-27 11:34:35 +0300

    delete image functionality added

--- backend/controllers/uploadController.js
+++ backend/controllers/uploadController.js
@@ -1,17 +1,83 @@
-const Image = require('../models/Image');
+const Image = require("../models/Image");
+const fs = require("fs");
+const path = require("path");
 
-const uploadImage = async (req, res) => {
+exports.uploadImage = async (req, res) => {
   try {
     const newImage = new Image({
       filename: req.file.filename,
     });
     await newImage.save();
-    res.status(200).json({ message: 'Dosya başarıyla yüklendi ve veritabanına kaydedildi', file: req.file });
+    res.status(200).json({
+      message: "Dosya başarıyla yüklendi ve veritabanına kaydedildi",
+      file: req.file,
+    });
   } catch (error) {
-    res.status(500).json({ message: 'Dosya yükleme veya veritabanına kaydetme hatası', error: error.message });
+    res.status(500).json({
+      message: "Dosya yükleme veya veritabanına kaydetme hatası",
+      error: error.message,
+    });
   }
 };
 
-module.exports = {
-  uploadImage,
+exports.getAllImages = async (req, res) => {
+  try {
+    const images = await Image.find();
+    res.status(200).json(images);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Görüntüleri alma hatası", error: error.message });
+  }
+};
+
+exports.deleteImage = async (req, res) => {
+  try {
+    const image = await Image.findById(req.params.id);
+    if (!image) {
+      return res.status(404).json({ message: "Görüntü bulunamadı" });
+    }
+
+    // Dosya sisteminden görüntüyü silme
+    const imagePath = path.join(__dirname, "../uploads/images", image.filename);
+    fs.unlink(imagePath, (err) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({
+            message: "Dosya silinirken hata oluştu",
+            error: err.message,
+          });
+      }
+    });
+
+    await Image.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Görüntü başarıyla silindi" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "Görüntü silinirken hata oluştu",
+        error: error.message,
+      });
+  }
+};
+
+exports.deleteImageNotPath = async (req, res) => {
+  try {
+    const image = await Image.findById(req.params.id);
+    if (!image) {
+      return res.status(404).json({ message: "Görüntü bulunamadı" });
+    }
+
+    await Image.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Görüntü başarıyla silindi" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "Görüntü silinirken hata oluştu",
+        error: error.message,
+      });
+  }
 };
--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const upload = require('../config/multerConfig');
-const { uploadImage } = require('../controllers/uploadController');
+const { uploadImage, getAllImages, deleteImage, deleteImageNotPath } = require('../controllers/uploadController');
 
 const router = express.Router();
 
 router.post('/upload', upload.single('file'), uploadImage);
-
+router.get('/images',getAllImages);
+router.delete('/images/:id', deleteImage); 
+router.delete('/resimler/:id', deleteImageNotPath); 
 module.exports = router;
